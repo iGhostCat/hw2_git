63d8e600593f3b15f7515b65872c9bb33b5f080d
iGhostCat <alfa-1002@mail.ru>
2025-04-25 12:56:50 +0300

    added sort by date function without typing

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
-def filter_by_state(dict_in,
-                    state=True):
+def filter_by_state(dict_in : list,
+                    state=True) -> list:
     ''' Функция фильтрации списка операций пользователя по статусу выполнения, принимает
         на вход список словарей операций, возвращает список словарей, отфильтрованный по
         значению статуса выполнения '''
@@ -14,3 +14,11 @@ def filter_by_state(dict_in,
         if dic['state'] == status:
             dict_out.append(dic)
     return dict_out
+
+
+def sort_by_date(dict_in : list, direction = True):
+    ''' Функция сортировки словарей в списке по дате, принимает список словарей и
+        необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+        Функция должна возвращать новый список, отсортированный по дате'''
+    sorted_dict = sorted(dict_in, key=lambda d: d['date'], reverse=direction)
+    return sorted_dict
