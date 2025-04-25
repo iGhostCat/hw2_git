4285330bce539d6683452e51a311942c20d50b85
iGhostCat <alfa-1002@mail.ru>
2025-04-25 11:42:29 +0300

    added function for filtering collections, flake8 checked

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,16 @@
-def filter_by_state():
+def filter_by_state(dict_in,
+                    state=True):
+    ''' Функция фильтрации списка операций пользователя по статусу выполнения, принимает
+        на вход список словарей операций, возвращает список словарей, отфильтрованный по
+        значению статуса выполнения '''
+    status = ''
+    if state:
+        status = 'EXECUTED'
+    else:
+        status = 'CANCELED'
+
+    dict_out = []
+    for dic in dict_in:
+        if dic['state'] == status:
+            dict_out.append(dic)
+    return dict_out
