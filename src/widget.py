e2cb2fbed435582094fdf141aa7b808b6ffdd68a
krol.lm <krol.lm@talantiuspeh.ru>
2025-05-03 15:05:06 +0300

    added tests for widget module

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,8 @@
-from masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(input_str : str) -> str:
+
     if 'Visa' in input_str:
         return get_mask_card_number(input_str[-16:])
     elif 'Master' in input_str:
@@ -9,12 +10,14 @@ def mask_account_card(input_str : str) -> str:
     elif 'Maestro' in input_str:
         return get_mask_card_number(input_str[-16:])
     elif 'Счет' in input_str:
-        return get_mask_account(input_str)
+        return get_mask_account(input_str[-20:])
     else:
         return 'Неверный ввод!'
 
 
 def get_date(date_iso : str) -> str:
+    if len(date_iso) == 0:
+        return'Неверный ввод!'
     year = date_iso[0:4]
     month = date_iso[5:7]
     day = date_iso[8:10]
---
+++ tests/test_widget.py
@@ -0,0 +1,22 @@
+from src.widget import mask_account_card
+from src.widget import get_date
+import pytest
+
+
+@pytest.mark.parametrize('input_str, expected', [('Maestro 1596837868705199', '1596 83** **** 5199'),
+                                                 ('Счет 64686473678894779589', '**9589'),
+                                                 ('MasterCard 7158300734726758', '7158 30** **** 6758'),
+                                                 ('Счет 35383033474447895560','**5560'),
+                                                 ('Visa Classic 6831982476737658','6831 98** **** 7658'),
+                                                 ('Visa Platinum 8990922113665229', '8990 92** **** 5229'),
+                                                 ('Visa Gold 5999414228426353', '5999 41** **** 6353'),
+                                                 ('Счет 73654108430135874305', '**4305'),
+                                                 ('', 'Неверный ввод!')])
+def test_mask_account_card(input_str,expected):
+    assert mask_account_card(input_str) == expected
+
+@pytest.mark.parametrize('date_iso, expected_date', [('2019-07-03T18:35:29.512364', '03.07.2019'),
+                                                     ('2018-06-30T02:08:58.425572', '30.06.2018'),
+                                                     ('', 'Неверный ввод!')])
+def test_get_date(date_iso,expected_date):
+    assert get_date(date_iso) == expected_date
