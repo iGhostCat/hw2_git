e2e3a08bcc0d9e037e11fe2bae4bdc8bfa90124e
krol.lm <krol.lm@talantiuspeh.ru>
2025-05-04 21:03:45 +0300

    added processing tests, formatted all files with black, test coverage 100%

--- src/processing.py
+++ src/processing.py
@@ -1,25 +1,25 @@
 from typing import List, Dict, Union
 
 
-def filter_by_state(list_input : List[Dict[str, Union[int, str, bool]]],
-                    state: str = 'EXECUTED') -> List[Dict[str, Union[int, str, bool]]]:
-    ''' Функция фильтрации списка операций пользователя по статусу выполнения, принимает
-        на вход список словарей операций, возвращает список словарей, отфильтрованный по
-        значению статуса выполнения '''
+def filter_by_state(
+    list_input: List[Dict[str, Union[int, str, bool]]], state: str = "EXECUTED"
+) -> List[Dict[str, Union[int, str, bool]]]:
+    """Функция фильтрации списка операций пользователя по статусу выполнения, принимает
+    на вход список словарей операций, возвращает список словарей, отфильтрованный по
+    значению статуса выполнения"""
 
     list_output = []
     for element in list_input:
-        if element['state'] == state:
+        if element["state"] == state:
             list_output.append(element)
     return list_output
 
 
 def sort_by_date(
-    list_input: List[Dict[str, Union[int, str, bool]]],
-    direction: bool = False
+    list_input: List[Dict[str, Union[int, str, bool]]], direction: bool = False
 ) -> List[Dict[str, Union[int, str, bool]]]:
-    ''' Функция сортировки словарей в списке по дате, принимает список словарей и
-        необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
-        Функция должна возвращать новый список, отсортированный по дате'''
-    sorted_list = sorted(list_input, key=lambda d: d['date'], reverse=direction)
+    """Функция сортировки словарей в списке по дате, принимает список словарей и
+    необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате"""
+    sorted_list = sorted(list_input, key=lambda d: d["date"], reverse=direction)
     return sorted_list
