d70205add15dbf8d8a1d51c3e2259022293578bb
krol.lm <krol.lm@talantiuspeh.ru>
2025-05-23 16:03:05 +0300

    added csv and xlsx files with transactions  for convertations and added csv_to_python function in utils

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,9 @@ import json
 import logging
 import os
 from pathlib import Path
+import csv
+import pandas as pd
+from collections import OrderedDict
 
 utils_logger = logging.getLogger("utils")
 utils_file_handler = logging.FileHandler("../logs/utils.log", encoding="utf-8", mode="w")
@@ -11,7 +14,31 @@ utils_logger.addHandler(utils_file_handler)
 utils_logger.setLevel(logging.DEBUG)
 
 
+def csv_to_list(path_to_file):
+    '''Функция, преобразовывающая содержимое файла .CSV в объект python
+       для дальнейшей обработки. Возвращает список словарей.'''
+    with open(path_to_file, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        result = []
+        for row in reader:
+            transaction = {
+                'id': row['id'],
+                'state': row['state'],
+                'date': row['date'],
+                'amount': row['amount'],
+                'currency_name': row['currency_name'],
+                'currency_code': row['currency_code'],
+                'from': row['from'],
+                'to': row['to'],
+                'description': row['description']
+            }
+            result.append(transaction)
+    return result
+#print(csv_to_list("../data/transactions.csv"))
+
 def json_to_list(file_path):
+    '''Функция, преобразовывающая содержимое файла .json в объект python
+       для дальнейшей обработки. Возвращает список словарей.'''
     path = Path(file_path)
     if not path.exists():
         utils_logger.error(f"Ошибка: по указанному пути {path} не найдено файла")
@@ -26,4 +53,4 @@ def json_to_list(file_path):
             return []
 
 
-print(json_to_list("../data/test_operations.json"))
+#print(json_to_list("../data/test_operations.json"))
