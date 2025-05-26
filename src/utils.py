23950b19095edce84328f2ac069df7a76bfa67f4
krol.lm <krol.lm@talantiuspeh.ru>
2025-05-26 18:59:05 +0300

    hw_13_1 completed, lintered all files

--- src/utils.py
+++ src/utils.py
@@ -2,43 +2,22 @@ import json
 import logging
 import os
 from pathlib import Path
-import csv
-import pandas as pd
-from collections import OrderedDict
+
+# Создаем папку logs
+log_dir = Path(__file__).parent.parent / "logs"
+log_dir.mkdir(exist_ok=True)
 
 utils_logger = logging.getLogger("utils")
-utils_file_handler = logging.FileHandler("../logs/utils.log", encoding="utf-8", mode="w")
+utils_file_handler = logging.FileHandler(log_dir / "utils.log", encoding="utf-8", mode="w")
 utils_file_formatter = logging.Formatter("%(asctime)s: %(filename)s: %(funcName)s: %(levelname)s: %(message)s")
 utils_file_handler.setFormatter(utils_file_formatter)
 utils_logger.addHandler(utils_file_handler)
 utils_logger.setLevel(logging.DEBUG)
 
 
-def csv_to_list(path_to_file):
-    '''Функция, преобразовывающая содержимое файла .CSV в объект python
-       для дальнейшей обработки. Возвращает список словарей.'''
-    with open(path_to_file, mode='r', encoding='utf-8') as file:
-        reader = csv.DictReader(file, delimiter=';')
-        result = []
-        for row in reader:
-            transaction = {
-                'id': row['id'],
-                'state': row['state'],
-                'date': row['date'],
-                'amount': row['amount'],
-                'currency_name': row['currency_name'],
-                'currency_code': row['currency_code'],
-                'from': row['from'],
-                'to': row['to'],
-                'description': row['description']
-            }
-            result.append(transaction)
-    return result
-#print(csv_to_list("../data/transactions.csv"))
-
 def json_to_list(file_path):
-    '''Функция, преобразовывающая содержимое файла .json в объект python
-       для дальнейшей обработки. Возвращает список словарей.'''
+    """Функция, преобразовывающая содержимое файла .json в объект python
+    для дальнейшей обработки. Возвращает список словарей."""
     path = Path(file_path)
     if not path.exists():
         utils_logger.error(f"Ошибка: по указанному пути {path} не найдено файла")
@@ -53,5 +32,5 @@ def json_to_list(file_path):
             return []
 
 
-#print(json_to_list("../data/test_operations.json"))
+# print(json_to_list("../data/test_operations.json"))
 #################
